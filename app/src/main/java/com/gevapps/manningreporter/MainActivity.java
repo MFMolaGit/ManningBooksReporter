9e7f3837ceefabbea1597252bfd03c22066b9b82
MFMolaGit <manuelf.mola@gmail.com>
2016-01-21 19:58:04 +0100

    Commit inicial

--- app/src/main/java/com/gevapps/manningreporter/MainActivity.java
+++ app/src/main/java/com/gevapps/manningreporter/MainActivity.java
@@ -2,13 +2,23 @@ package com.gevapps.manningreporter;
 
 import android.app.*;
 import android.os.*;
+import android.widget.EditText;
+import android.widget.*;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends Activity {
+
+	private EditText dateFilter, textFilter;
+	private ScrollView offersView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		textFilter = (EditText) findViewById(R.id.textFilter);
+		dateFilter = (EditText) findViewById(R.id.dateFilter);
+		offersView = (ScrollView) findViewById(R.id.offersView);
+
+
     }
 }
